3af7256343de25c95045c4d05ac938813a033662
creativerze <creativerze@gmail.com>
2023-10-30 00:49:52 +0600

    added forgotPassword and resetPassword

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -30,15 +30,14 @@ const refreshToken = async (req: Request, res: Response, next: NextFunction) => 
   try {
     const result = await AuthenticationService.refreshToken(req);
 
-    // const { refreshToken, ...others } = result.data;
-    const { ...others } = result.data;
+    const { refreshToken, ...others } = result.data;
 
-    // const cookieOptions = {
-    //   secure: config.env === 'production',
-    //   httpOnly: true
-    // };
+    const cookieOptions = {
+      secure: config.env === 'production',
+      httpOnly: true
+    };
 
-    // res.cookie('refreshToken', result.data.refreshToken, cookieOptions);
+    res.cookie('refreshToken', result.data.refreshToken, cookieOptions);
     sendResponse(res, {
       statusCode: httpStatus.OK,
       success: true,
@@ -58,9 +57,28 @@ const changePassword = async (req: Request, res: Response, next: NextFunction) =
     next(error);
   }
 };
+const forgotPassword = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AuthenticationService.forgotPassword(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await AuthenticationService.resetPassword(req);
+    sendResponse(res, result);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const AuthenticationController = {
   loginUser,
   refreshToken,
-  changePassword
+  changePassword,
+  forgotPassword,
+  resetPassword
 };
--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -8,26 +8,29 @@ import { ENUM_USER_ROLE } from '../../../enums/user';
 const router = express.Router();
 
 router.post(
-    '/login',
-    validateRequest(AuthValidation.loginZodSchema),
-    AuthenticationController.loginUser
+  '/login',
+  validateRequest(AuthValidation.loginZodSchema),
+  AuthenticationController.loginUser
 );
 router.post(
-    '/refresh-token',
-    validateRequest(AuthValidation.refreshTokenZodSchema),
-    AuthenticationController.refreshToken
+  '/refresh-token',
+  validateRequest(AuthValidation.refreshTokenZodSchema),
+  AuthenticationController.refreshToken
 );
 
+router.post('/forgot-password', AuthenticationController.forgotPassword);
+router.post('/reset-password', AuthenticationController.resetPassword);
+
 router.post(
-    '/change-password',
-    auth(
-        ENUM_USER_ROLE.SUPER_ADMIN,
-        ENUM_USER_ROLE.ADMIN,
-        ENUM_USER_ROLE.STUDENT,
-        ENUM_USER_ROLE.FACULTY
-    ),
-    validateRequest(AuthValidation.changePasswordZodSchema),
-    AuthenticationController.changePassword
+  '/change-password',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.STUDENT,
+    ENUM_USER_ROLE.FACULTY
+  ),
+  validateRequest(AuthValidation.changePasswordZodSchema),
+  AuthenticationController.changePassword
 );
 
 export const authRoutes = router;
